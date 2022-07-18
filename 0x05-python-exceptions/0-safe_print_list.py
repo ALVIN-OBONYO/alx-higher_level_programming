c781231a2b47ba6295f843d2fef8b15b2fa76685
ALVIN-OBONYO <ALVIN-OBONYO>
2022-07-18 16:57:14 -0700

    pop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-        i = 0
         try:
-            while i is not x:
+            for i in range(x):
                print(my_list[i], end='')
-               i += 1
           except IndexError:
-              None
+              print()
+              return 1
          print()
-         return 1
+         return x
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
-        try:
-           print("{:d}".format(value))
-           return True
-       except (ValueError, TypeError):
-           return False
+    """
+     Prints an integer with "{:d}".format()
+
+     Args:
+        value: value to be printed
+
+     Return: True if successful.Otherwise False 
+     """
+     try:
+         print("{:d}".format(value))
+     except (ValueError, TypeError):
+         return False
+     else:
+         return True
+        
