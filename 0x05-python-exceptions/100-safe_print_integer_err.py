36257d186188ead1d21fa7f94280a263e35831f2
ALVIN-OBONYO <ALVIN-OBONYO>
2022-07-19 04:03:31 -0700

    last

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-        try:
-            print("{:d}".format(value))
-            return True
-        except Exception as e:
-            print("Exception: " + e.args[0], file=sys.stderr)
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+   except Exception as e:
+       print("Exception: " + e.args[0], file=sys.stderr)
+       return False
