0df36979e8361b2c97a3358ea827d881baaa4880
Alvin Obonyo <41358283+ALVIN-OBONYO@users.noreply.github.com>
2022-07-27 22:43:52 +0300

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Module 0-rectangle
-Defines an empty Rectangle class.
-"""
+""" Defining an empty class rectangle """
 
 
 class Rectangle:
-        """Empty class."""
-            pass
+    """ Defining a rectangle """
+    pass
