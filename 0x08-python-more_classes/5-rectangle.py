7946cbfdce426a6c3436a52ef88371e17171d153
Alvin Obonyo <41358283+ALVIN-OBONYO@users.noreply.github.com>
2022-07-27 22:55:34 +0300

    Update 5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,119 +1,82 @@
 #!/usr/bin/python3
 """
-Contains a definition for class rectangle.
+    Defines a Rectangle class
 """
 
 
 class Rectangle:
-    """Definition of class rectangle.
-       Attributes:
-           width(int): rectangle width.
-           height(int): rectangle height.
     """
+        defines a Rectangle class
+     """
     def __init__(self, width=0, height=0):
+        """Instatiates class with optinal attributes.
+            atttributes:
+                width - the width of the rectangle
+                height - the height of the ractangle
         """
-            Initializes a new Class Rectangle instance.
-            Args:
-                width(int): rectangle width.
-                height(int): rectangle height.
-            Raises:
-                TypeError: if width/height is not int.
-                ValueError: if width/ height is not >= 0
-        """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
         self.__width = width
-
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
     def width(self):
-        """
-            getter function for private attribute width.
-            Returns: width of the rectangle.
-        """
+        """ retrieve the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-            setter function for private attribute width.
-            Args:
-                value(int) new width value.
-            Raises:
-                TypeError: if value is not int.
-                ValueError: if value is not >= 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("value must be an integer")
-        elif value < 0:
-            raise ValueError("value must be >= 0")
-        self.__width = value
+        """ Sets the width of the reactangle"""
+        if value < 0:
+            raise ValueError("width must be >=0")
+        if (not isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        """
-            getter function for private attribute height.
-            Returns: height of the triangle.
-        """
+        """Retirves the height of rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-            setter function for private attribute height.
-            Args:
-                value(int) new width value.
-            Raises:
-                TypeError: if value is not int.
-                ValueError: if value is not >= 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("value must be an integer")
-        elif value < 0:
-            raise ValueError("value must be >= 0")
-        self.__height = value
+        """Sets the height of rectangle"""
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if (not isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        else:
+            self.__height = value
 
     def area(self):
-        """Public instance method to compute area of rectangle.
-           Returns: area of rectangle.
-        """
-        return self.__width * self.__height
+        """Returns the area of the rectangle"""
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        """Public instance method to compute perimeter of triangle.
-           Returns: perimeter of triangle.
-        """
+        """returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+            return (0)
+        else:
+            return (2 * (self.__width + self.__height))
 
     def __str__(self):
-        """returns string representation of a rectangle.
-        """
-        rect = []
-        if self.__height == 0 or self.__width == 0:
-            return ""
-        for n in range(0, self.__height):
-            rect.append("#" * self.__width)
-            if n != self.__height - 1:
-                rect.append('\n')
-        return ''.join(rect)
+        """prints object as string"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        else:
+            rect = []
+            for h in range(self.__height):
+                [rect.append("#") for w in range(self.__width)]
+                if h != self.__height - 1:
+                    rect.append("\n")
+            return ("".join(rect))
 
     def __repr__(self):
-        """Return string representation of rectangle.
-            Should be able to create a new instance using eval().
-        """
+        """return a string representation of rectangle"""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return rect
 
-        return '{self.__class__.__name__}({self.width}, {self.height})'.\
-            format(self=self)
     def __del__(self):
-        """Prints string to STDOUT when rectangle object is deleted"""
-
+        """garbage collector that destroys an object"""
         print("Bye rectangle...")
